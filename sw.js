24e17a1605ebbd5bc595db3788d0fc9e9df2baaa
sandamal <sandamal0088@gmail.com>
2026-07-24 23:37:47 +0530

    Replace index.html with daily_roster.html in cache

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install",e=>{
  e.waitUntil(
   caches.open(CACHE).then(c=>c.addAll([
    "./",
-   "./index.html",
+   "./daily_roster.html",
    "./manifest.json"
   ]))
  );
